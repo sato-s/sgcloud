de129454c4e10a498c64209fcdbec73a835d2d42
sato-s <s.sato.desu@gmail.com>
2024-08-03 18:42:33 +0900

    gcloud project listを直接打つようにした

--- internal/command/command.go
+++ internal/command/command.go
@@ -1,5 +1,17 @@
 package command
 
+import (
+	"github.com/pterm/pterm"
+	"os/exec"
+)
+
 func RunGcloud() error {
+	out, err := exec.Command("gcloud", "projects", "list").Output()
+	if err != nil {
+		return err
+	} else {
+		pterm.Info.Println(string(out))
+	}
+
 	return nil
 }
